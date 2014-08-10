89f0485174c102c7b4c43e4d6ad559195b7f2753
wangkp <kevin.kaipeng.wang@gmail.com>
2014-08-10 16:15:55 -0400

    project1

--- plot3.R
+++ plot3.R
@@ -8,10 +8,10 @@ newd<-subset(data, data$Date=="1/2/2007"|data$Date=="2/2/2007")
 newd$Datetime <- strptime(paste(newd$Date, newd$Time), "%d/%m/%Y %H:%M:%S")
 
 ## create plot3
+plot("plot3.png",width=480,height=480)
 plot(newd$Datetime, newd$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 plot(newd$Datetime, newd$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 points(newd$Datetime, newd$Sub_metering_2, type = "l", xlab = "", ylab = "Energy sub metering", col = "red")
 points(newd$Datetime, newd$Sub_metering_3, type = "l", xlab = "", ylab = "Energy sub metering", col = "blue")
 legend("topright", lty = 1, col = c("black", "red", "blue"),  legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-## When I used Rstudio to export is as 480*480, I found some text was left out in the photo. I also added text.width=50000 as some people suggested in the forum. But again, it didn't work.Therefore, I decided to export it as the default size. And then I modified the picture using sips command line on Terminal. PLEASE WRITE DOWN HOW YOU DEALT WITH THE ISSUE IF YOU HAVE BETTER SOLUTION. THANKS!
+dev.off()
--- plot4.R
+++ plot4.R
@@ -7,6 +7,7 @@ newd<-subset(data, data$Date=="1/2/2007"|data$Date=="2/2/2007")
 ## perform conversions on data and time values
 newd$Datetime <- strptime(paste(newd$Date, newd$Time), "%d/%m/%Y %H:%M:%S")
 
+png("plot4.png",width=480,height=480)
 par(mfrow = c(2, 2))
 
 ## plot NW
@@ -22,6 +23,6 @@ points(newd$Datetime, newd$Sub_metering_2, type = "l", xlab = "", ylab = "Sub_me
 points(newd$Datetime, newd$Sub_metering_3, type = "l", xlab = "", ylab = "Sub_metering_3", col = "blue")
 legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n", )
 
-## plot 4 SE
+## plot SE
 plot(newd$Datetime, newd$Global_reactive_power, type = "l", xlab = "datetime", 
     ylab = "Global_reactive_power", ylim = c(0, 0.5))
